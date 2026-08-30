fd53b37b68769f8ad39d1e07b8ea6db2118d4e01
riku-penguin <riku20090730@gmail.com>
2026-08-30 22:46:36 +0900

    Update player.jsaaaa

--- player.js
+++ player.js
@@ -1,5 +1,6 @@
 // スプレッドシート連携URL
-There was an error committing your changes: riku-penguin has committed since you started editing
+const SHEET_URL = "https://withered-dawn-9880.riku20090730.workers.dev";
+
 
 // 共有用リンク（ベースURL）
 const SHARE_URL = "https://riku-penguin.github.io/tankyuu-survey/";
